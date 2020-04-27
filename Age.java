ea2fbf8fa6e663162d94870ed4b447a3bbb2b620
ELBASKO64 <64418498+ELBASKO64@users.noreply.github.com>
2020-04-27 17:21:47 +0200

    Add files via upload

---
+++ Age.java
@@ -0,0 +1,31 @@
+package fr.elbasko.programme;
+
+public class Main {
+
+	public static void main(String[] arg) {
+		
+	int age = 9;
+	
+	if(age >=18){
+		System.out.println("le personne est majeur");
+		
+	} else if(age <= 10){
+		System.out.println("la personne est mineur et elle est tres jeune");
+		
+	} else {
+		System.out.println("la personne n'est pas majeur");
+		
+	}
+	
+	}
+	
+}
+	
+		
+
+	
+		
+
+	
+
+
