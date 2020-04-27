7eefd074a8b040cdaa896528bd259784f71b3c51
ELBASKO64 <64418498+ELBASKO64@users.noreply.github.com>
2020-04-27 17:22:40 +0200

    Add files via upload

---
+++ Moyenne.java
@@ -0,0 +1,27 @@
+package fr.elbasko.programme;
+
+public class Main {
+
+	public static void main(String[] arg) {
+		
+	int note1 = 10;
+	int note2 = 20;
+	int note3 = 30;
+	
+	int calcul = (note1+note2+note3) / 3;
+		
+	System.out.println(calcul);
+		
+	}
+	
+	}
+	
+
+		
+
+	
+		
+
+	
+
+
