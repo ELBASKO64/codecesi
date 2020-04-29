25aef7d26e8f1770ae079f5fa4c539d7cedb6c19
ELBASKO64 <64418498+ELBASKO64@users.noreply.github.com>
2020-04-29 17:48:23 +0200

    Add files via upload

---
+++ convertircaracteretableau.java
@@ -0,0 +1,22 @@
+package fr.elbasko.programme;
+
+public class Main {
+
+	public static void main(String[] arg) {
+		
+	
+	String pseudos = "elbasko,yannou,footeux,gameurs";
+	String[] pseudo = pseudos.split(",");
+	System.out.println(pseudo.length);
+		
+	
+	
+			
+	}
+	
+	}
+	
+
+		
+
+	
