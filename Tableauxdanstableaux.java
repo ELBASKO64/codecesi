e5ae5606983807e5c18a6853452d0c2363d3018f
ELBASKO64 <64418498+ELBASKO64@users.noreply.github.com>
2020-04-29 17:33:29 +0200

    Add files via upload

---
+++ Tableauxdanstableaux.java
@@ -0,0 +1,47 @@
+package fr.elbasko.programme;
+
+public class Main {
+
+	public static void main(String[] arg) {
+		
+	int note1 = 10;
+	int note2 = 20;
+	int note3 = 30;
+	
+	int calcul = (note1+note2+note3) / 3;
+		
+	System.out.println(calcul);
+		
+	
+	
+	int[][] numbers = { 
+			{ 10, 11, 12
+				
+	},
+			{ 20, 21, 21
+		
+			},
+			
+			{ 20, 21, 21
+				
+			}
+			
+			};
+	System.out.println(numbers[2][2]);
+	
+	
+	
+			
+	}
+	
+	}
+	
+
+		
+
+	
+		
+
+	
+
+
