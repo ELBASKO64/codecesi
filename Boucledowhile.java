205f4a407a7b484c452cfdfcfe8c388093aba4eb
ELBASKO64 <64418498+ELBASKO64@users.noreply.github.com>
2020-05-01 19:37:52 +0200

    Add files via upload

---
+++ Boucledowhile.java
@@ -0,0 +1,30 @@
+package fr.elbasko.programme;
+
+public class Main {
+
+	public static void main(String[] arg) {
+		
+	boolean europaleague = false;
+	
+	do {
+		System.out.println("ligue des champions");
+		
+	}while (!europaleague);
+	
+	
+
+	
+			
+	}
+	
+	}
+	
+
+		
+
+	
+		
+
+	
+
+
